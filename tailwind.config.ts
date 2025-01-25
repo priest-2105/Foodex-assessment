27b35404c3a8e6a7ba0e444570338ab767887d3a
priest-2105 <priest2105@gmail.com>
2025-01-25 20:25:17 +0100

    Init

---
+++ tailwind.config.ts
@@ -0,0 +1,13 @@
+import defaultTheme from 'tailwindcss/defaultTheme';
+
+export default {
+  content: ['./index.html', './src/**/*.{ts,tsx}'],
+  theme: {
+    extend: {
+      fontFamily: {
+        sans: ['InterVariable', ...defaultTheme.fontFamily.sans],
+      },
+    },
+  },
+  plugins: [],
+};
