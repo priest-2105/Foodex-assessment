d0c52dee01b3d342041d2ea0b678e5ee5df6b177
priest-2105 <priest2105@gmail.com>
2025-01-28 20:54:33 +0100

    build deployment

--- src/components/home/features.tsx
+++ src/components/home/features.tsx
@@ -2,14 +2,14 @@ import GemIcon from "../../assets/icons/gem"
 import Cloud from "../../assets/vectors/cloud"
 import WideCloud from "../../assets/vectors/wide-cloud"
 import featureImage from "../../assets/img/emmanuel-ikwuegbu-81fRHbVliQI-unsplash 1.png"
-import { FoodGrid } from "./food-grid";
+import { FoodGrid } from "./food-grid"
 import backgroundImage from "../../assets/img/features-background.png"
 
 const tagsData = [
-  { icon: '👍🏾', text: "Picked by 314 people today" },
-  { icon: '🧅', text: "Exclude onions" },
-  { icon: '🥩', text: "Must include turkey" },
-];
+  { icon: "👍🏾", text: "Picked by 314 people today" },
+  { icon: "🧅", text: "Exclude onions" },
+  { icon: "🥩", text: "Must include turkey" },
+]
 
 export function FeatureSection() {
   return (
@@ -18,100 +18,105 @@ export function FeatureSection() {
       <div className="absolute inset-0 bg-gradient-to-br from-orange-50/30 to-transparent" />
 
       {/* Content Container */}
-      <div className="relative z-10 max-w-7xl mx-auto px-4">
+      <div className="relative z-10 max-w-fit mx-auto px-4">
         {/* Header */}
         <div className="flex items-center justify-center space-x-3 mb-12">
-          <span className="text-black text-2xl"><GemIcon className="w-4 h-4" /></span>
-          <span className="uppercase tracking-[0.2em] text-sm font-pangram  font-medium">JOIN THE CHOWAFRICA WAITLIST</span>
-          <span className="text-black text-2xl"><GemIcon className="w-4 h-4" /></span>
+          <span className="text-black text-2xl">
+            <GemIcon className="w-4 h-4" />
+          </span>
+          <span className="uppercase tracking-[0.2em] text-sm font-pangram  font-medium">
+            JOIN THE CHOWAFRICA WAITLIST
+          </span>
+          <span className="text-black text-2xl">
+            <GemIcon className="w-4 h-4" />
+          </span>
         </div>
 
         {/* Main Content */}
         <div className="text-center mb-8 relative max-lg:hidden">
-        <div className="absolute bottom-0 left-20 z-0 max-lg:-left-50 max-lg:bottom-20"><WideCloud/></div>
-          <div className="absolute bottom-0 right-0 z-0  max-lg:-left-50 max-lg:bottom-20"><Cloud/></div>
-                    <h2 className="text-[#353542] text-6xl font-pangram font-bold mb-4 z-10">
-            tired of eating the
-          </h2>
-           <div className="inline-block relative z-10">
+          <div className="absolute bottom-0 left-20 z-0 max-lg:-left-50 max-lg:bottom-20">
+            <WideCloud className="opacity-50" />
+          </div>
+          <div className="absolute bottom-0 right-0 z-0 max-lg:-left-50 max-lg:bottom-20">
+            <Cloud className="opacity-50" />
+          </div>
+          <div className="relative z-10">
+            <h2 className="text-[#353542] text-6xl font-pangram font-bold mb-4">tired of eating the</h2>
+            <div className="inline-block">
               <span className="relative z-10 bg-[#E67E22] border-black border-4 text-white text-4xl px-8 py-2 font-sofia-sans rounded-full">
-              🍛 same dishes everyday? 🍝
+                🍛 same dishes everyday? 🍝
               </span>
             </div>
-          <p className="text-black max-w-xl my-12 font-semibold text-lg font-sofia-sans-condenced mx-auto z-10">
-            Do you crave new and exciting dishes? Look no further! Chow is the perfect solution for discovering your
-            next dish.
-          </p>
+            <p className="text-black max-w-xl my-12 font-semibold text-lg font-sofia-sans-condenced mx-auto">
+              Do you crave new and exciting dishes? Look no further! Chow is the perfect solution for discovering your
+              next dish.
+            </p>
+          </div>
         </div>
         <div className="text-center mb-8 relative max-lg:block lg:hidden">
-        <div className="absolute bottom-0 left-20 z-0 max-lg:-left-50 max-lg:bottom-20"><WideCloud/></div>
-          <div className="absolute bottom-0 right-0 z-0  max-lg:-left-50 max-lg:bottom-20"><Cloud/></div>
-                    <h2 className="text-[#353542] text-6xl max-lg:text-3xl font-pangram font-bold mb-4 z-10">
-            tired of eating the same dishes 
-             <span className="relative z-10 bg-[#E67E22] border-black border-4 text-white text-4xl px-8 py-2 font-sofia-sans rounded-full max-lg:mx-2 max-lg:text-3xl max-md:text-2xl max-sm:text-lg">
-               everyday? 
+          <div className="absolute bottom-0 left-20 z-0 max-lg:-left-50 max-lg:bottom-20">
+            <WideCloud className="opacity-50" />
+          </div>
+          <div className="absolute bottom-0 right-0 z-0 max-lg:-left-50 max-lg:bottom-20">
+            <Cloud className="opacity-50" />
+          </div>
+          <div className="relative z-10">
+            <h2 className="text-[#353542] text-6xl max-lg:text-3xl font-pangram font-bold mb-4">
+              tired of eating the same dishes
+              <span className="relative z-10 bg-[#E67E22] border-black border-4 text-white text-4xl px-8 py-2 font-sofia-sans rounded-full max-lg:mx-2 max-lg:text-3xl max-md:text-2xl max-sm:text-lg">
+                everyday?
               </span>
-          </h2>
-          <p className="text-black max-w-xl my-12 font-semibold text-lg font-sofia-sans-condenced mx-auto z-10 max-sm:px-4 max-lg:px-8">
-            Do you crave new and exciting dishes? Look no further! Chow is the perfect solution for discovering your
-            next dish.
-          </p>
+            </h2>
+            <p className="text-black max-w-xl my-12 font-semibold text-lg font-sofia-sans-condenced mx-auto max-sm:px-4 max-lg:px-8">
+              Do you crave new and exciting dishes? Look no further! Chow is the perfect solution for discovering your
+              next dish.
+            </p>
+          </div>
         </div>
 
         {/* Features Grid */}
         <div className="xl:ml-64 md:px-24">
-        <div className={`grid xl:grid-cols-2 gap-12 items-center mt-16 mx-auto bg-no-repeat bg-center bg-cover p-8`} style={{ backgroundImage: `url(${backgroundImage})` }}>
-          {/* Left Side - Food Grid */}
-           <FoodGrid/>
+          <div
+            className={`grid xl:grid-cols-2 gap-12 items-center mt-16 mx-auto bg-no-repeat bg-center bg-cover p-8`}
+            style={{ backgroundImage: `url(${backgroundImage})` }}
+          >
+            {/* Left Side - Food Grid */}
+            <FoodGrid />
 
-          {/* Right Side - Image and Features */}
-          <div className="relative justify-center w-fit  mx-auto text-center">
-            <img
-              src={featureImage}
-              alt="Person using phone"
-              className="rounded-2xl w-[397px] text-center md:ml-auto mx-auto"
-            />
+            {/* Right Side - Image and Features */}
+            <div className="relative justify-center w-fit mx-auto text-center">
+              <img
+                src={featureImage || "/placeholder.svg"}
+                alt="Person using phone"
+                className="rounded-2xl w-[397px] text-center md:ml-auto mx-auto"
+              />
 
-            {/* Feature Pills */}
-            <div className="absolute top-30 -right-26 max-md:-right-4 max-md:top-20 flex flex-col gap-2">
-              <div
-              className={`
-                bg-[#DB7023] text-white px-4 py-2 rounded-lg border-2 border-white 
-                block items-center gap-4 shadow-lg 
-              `}
-            >
-              <div className="bg-[#77441E] w-fit text-[9px]  p-1 mb-1 rounded-full">
-                💵
+              {/* Feature Pills */}
+              <div className="absolute top-30 -right-26 max-md:-right-4 max-md:top-20 flex flex-col gap-2">
+                <div className="bg-[#DB7023] text-white px-4 py-2 rounded-lg border-2 border-white block items-center gap-4 shadow-lg">
+                  <div className="bg-[#77441E] w-fit text-[9px] p-1 mb-1 rounded-full">💵</div>
+                  <p className="text-[9px] font-medium">
+                    You can buy all for<span className="text-[#2EE6A8] font-bold"> N2,500</span> at Yaba
+                  </p>
+                </div>
               </div>
-              <p className="text-[9px] font-medium">
-              You can buy all for<span className="text-[#2EE6A8]  font-bold"> N2,500</span>  at Yaba
-              </p>
-            </div>
-            </div>
 
-            <div className="absolute bottom-14 -left-4 flex flex-col gap-2">
-              
-              {tagsData.map((tag, index) => (
-                 <div
-                 className={`
-                   bg-[#009F79] text-white px-4 py-2 rounded-lg border-2 border-white 
-                   flex items-center gap-4 shadow-lg mt-[${index * 100}px]
-                 `}
-               >
-                 <div className="bg-[#1E735E] w-fit text-[9px]  p-1 mb-1 rounded-full">
-                 {tag.icon}
-                 </div>
-                 <p className="text-[9px] font-medium"> {tag.text}
-                 </p>
-               </div>
-              ))}
+              <div className="absolute bottom-14 -left-4 flex flex-col gap-2">
+                {tagsData.map((tag, index) => (
+                  <div
+                    key={index}
+                    className="bg-[#009F79] text-white px-4 py-2 rounded-lg border-2 border-white flex items-center gap-4 shadow-lg mt-2"
+                  >
+                    <div className="bg-[#1E735E] w-fit text-[9px] p-1 mb-1 rounded-full">{tag.icon}</div>
+                    <p className="text-[9px] font-medium">{tag.text}</p>
+                  </div>
+                ))}
+              </div>
             </div>
- 
           </div>
-        </div>
         </div>
       </div>
     </section>
-  );
+  )
 }
 
